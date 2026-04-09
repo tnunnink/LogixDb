9de9ea7849dfc3f606cf844b0d09db0995c2a47e
tnunnink <timothyjnunnink@gmail.com>
2026-04-08 19:53:42 -0500

    Revise `SqlServerDbMigrateTests` for better clarity and consistency: rename test method, replace `DbConnection` with `DbConnectionInfo`, update options (`DbOptions` to `TableOptions`), and refine test assertions.

--- test/LogixDb.Data.SqlServer.Tests/SqlServerDbMigrateTests.cs
+++ test/LogixDb.Data.SqlServer.Tests/SqlServerDbMigrateTests.cs
@@ -3,11 +3,15 @@ namespace LogixDb.Data.SqlServer.Tests;
 [TestFixture]
 public class SqlServerMigrationTest : SqlServerTestFixture
 {
+    /// <summary>
+    /// This test is mostly just to refresh a local db instance to inspect and write queries against.
+    /// The base test fixture will create and migrate the database
+    /// </summary>
     [Test]
     [Explicit("Manually run against local test server to check migrations and develop SQL queries against")]
-    public async Task Build_WithLocalContainerServer_ShouldCreateAndMigrateDatabase()
+    public async Task MigrateLocalTestDatabaseForWritingQueriesAgainst()
     {
-        var connectionInfo = new DbConnection(
+        var connectionInfo = new DbConnectionInfo(
             Provider: DbProvider.SqlServer,
             Source: "localhost,1433",
             Database: "logixdb",
@@ -17,44 +21,83 @@ public class SqlServerMigrationTest : SqlServerTestFixture
         );
 
         var database = new SqlServerDb(connectionInfo);
-
+        await database.Drop();
         await database.Migrate();
     }
     
     [Test]
     public async Task Migrate_IncludeSpecificTableNames_ShouldOnlyHaveConfiguredTables()
     {
-        var options = new DbOptions { Include = ["controller", "tag"] };
+        var options = new TableOptions { Include = ["controller", "tag"] };
         
         await Database.Migrate(options);
 
+        // Required Tables
         await AssertTableExists("target");
         await AssertTableExists("snapshot");
+        await AssertTableExists("snapshot_property");
         await AssertTableExists("operand");
+
+        // Included Tables
         await AssertTableExists("controller");
         await AssertTableExists("tag");
-        await AssertTableDoesNotExists("aoi");
-        await AssertTableDoesNotExists("aoi_parameter");
-        await AssertTableDoesNotExists("aoi_logic");
+        await AssertTableExists("tag_member");
+        await AssertTableExists("tag_comment");
+        await AssertTableExists("tag_producer");
+        await AssertTableExists("tag_consumer");
+        await AssertTableExists("tag_alias");
+        
+        // Excluded Tables
         await AssertTableDoesNotExists("data_type");
         await AssertTableDoesNotExists("data_type_member");
+        await AssertTableDoesNotExists("module");
         await AssertTableDoesNotExists("task");
         await AssertTableDoesNotExists("program");
         await AssertTableDoesNotExists("routine");
         await AssertTableDoesNotExists("rung");
         await AssertTableDoesNotExists("instruction");
+        await AssertTableDoesNotExists("argument");
+        await AssertTableDoesNotExists("aoi");
+        await AssertTableDoesNotExists("aoi_parameter");
+        await AssertTableDoesNotExists("aoi_rung");
     }
 
     [Test]
     public async Task Migrate_ExcludeSpecificTableNames_ShouldNotHaveExcludedTables()
     {
-        var options = new DbOptions { Exclude = ["instruction", "rung"] };
+        var options = new TableOptions { Exclude = ["instruction", "rung"] };
 
         await Database.Migrate(options);
 
+        // Required Tables
+        await AssertTableExists("target");
+        await AssertTableExists("snapshot");
+        await AssertTableExists("snapshot_property");
+        await AssertTableExists("operand");
+
+        // Included Tables (not explicitly excluded, and Include is empty)
         await AssertTableExists("controller");
+        await AssertTableExists("data_type");
+        await AssertTableExists("data_type_member");
+        await AssertTableExists("module");
+        await AssertTableExists("task");
+        await AssertTableExists("program");
+        await AssertTableExists("routine");
+        await AssertTableExists("aoi");
+        await AssertTableExists("aoi_parameter");
+        await AssertTableExists("aoi_rung");
         await AssertTableExists("tag");
-        await AssertTableDoesNotExists("instruction");
+        await AssertTableExists("tag_member");
+        await AssertTableExists("tag_comment");
+        await AssertTableExists("tag_producer");
+        await AssertTableExists("tag_consumer");
+        await AssertTableExists("tag_alias");
+        
+        // Excluded Tables
+        // Since "rung" tag is excluded, all tables with "rung" tag should be missing
+        // "rung", "instruction", "argument" all have MigrationTag.Rung
         await AssertTableDoesNotExists("rung");
+        await AssertTableDoesNotExists("instruction");
+        await AssertTableDoesNotExists("argument");
     }
 }

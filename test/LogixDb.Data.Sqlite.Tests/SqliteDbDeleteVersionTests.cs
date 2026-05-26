c35f7f5c512e0f4301b6abbc71e4b0b4a00cc8e4
tnunnink <timothyjnunnink@gmail.com>
2026-05-26 11:35:05 -0500

    Add `DeleteVersion.sql` script, corresponding tests for SQL Server and SQLite, and update `.idea` configuration to reflect changes.

---
+++ test/LogixDb.Data.Sqlite.Tests/SqliteDbDeleteVersionTests.cs
@@ -0,0 +1,53 @@
+using LogixDb.Data;
+using LogixDb.Testing;
+
+namespace LogixDb.Data.Sqlite.Tests;
+
+[TestFixture]
+public class SqliteDbDeleteVersionTests : SqliteTestFixture
+{
+    [SetUp]
+    protected async Task Setup()
+    {
+        await Database.Migrate();
+    }
+
+    [Test]
+    public async Task DeleteVersion_SpecificVersion_ShouldRemoveOnlyThatVersion()
+    {
+        var target1 = Target.Create(TestSource.LocalTest());
+        await Database.ImportTarget(target1); // Version 1
+
+        var target2 = Target.Create(TestSource.LocalTest());
+        await Database.ImportTarget(target2); // Version 2
+
+        await Database.DeleteVersion(target1.TargetKey, 1);
+
+        var result = (await Database.ListTargets()).ToArray();
+        Assert.That(result, Has.Length.EqualTo(1));
+        Assert.That(result[0].VersionNumber, Is.EqualTo(2));
+    }
+
+    [Test]
+    public async Task DeleteVersion_MultipleTargets_ShouldOnlyRemoveTargetVersion()
+    {
+        var target1 = Target.Create(TestSource.LocalTest());
+        await Database.ImportTarget(target1);
+
+        var target2 = Target.Create(TestSource.LocalTest(), "OtherTarget");
+        await Database.ImportTarget(target2);
+
+        await Database.DeleteVersion(target1.TargetKey, 1);
+
+        var result = (await Database.ListTargets()).ToArray();
+        Assert.That(result, Has.Length.EqualTo(1));
+        Assert.That(result[0].TargetKey, Is.EqualTo(target2.TargetKey));
+    }
+
+    [Test]
+    public Task DeleteVersion_NonExistent_ShouldNotThrow()
+    {
+        Assert.DoesNotThrowAsync(async () => await Database.DeleteVersion("NonExistent", 1));
+        return Task.CompletedTask;
+    }
+}

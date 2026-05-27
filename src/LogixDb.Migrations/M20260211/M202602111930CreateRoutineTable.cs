ae8cff257dc79e75f8c49175917d95fdd6ab3ca3
tnunnink <timothyjnunnink@gmail.com>
2026-05-27 09:56:30 -0500

    Remove `aoi_rung` table migration and update `routine` table with `container_name` and `is_definition` columns.

--- src/LogixDb.Migrations/M20260211/M202602111930CreateRoutineTable.cs
+++ src/LogixDb.Migrations/M20260211/M202602111930CreateRoutineTable.cs
@@ -13,10 +13,11 @@ public class M202602111930CreateRoutineTable : AutoReversingMigration
     {
         Create.Table("routine")
             .WithPrimaryKey<long>("routine_id")
-            .WithColumn("program_name").AsString(256).NotNullable()
+            .WithColumn("container_name").AsString(256).NotNullable()
             .WithColumn("routine_name").AsString(256).NotNullable()
             .WithColumn("routine_description").AsString(512).Nullable()
             .WithColumn("routine_type").AsString(32).Nullable()
+            .WithColumn("is_definition").AsBoolean().NotNullable()
             .WithColumn("content_hash").AsString(64).NotNullable()
             .WithColumn("record_hash").AsString(64).NotNullable();
 
@@ -25,7 +26,7 @@ public class M202602111930CreateRoutineTable : AutoReversingMigration
             .WithOptions().Unique();
 
         Create.Index().OnTable("routine")
-            .OnColumn("program_name").Ascending()
+            .OnColumn("container_name").Ascending()
             .OnColumn("routine_name").Ascending();
 
         Create.Index().OnTable("routine")

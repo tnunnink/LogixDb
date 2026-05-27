01bffabdce02d29b73f8fb3d2b8454a6ea51e809
tnunnink <timothyjnunnink@gmail.com>
2026-05-27 10:01:09 -0500

    Remove `AoiRungMap` and associated references from `AoiTransformer` to simplify AOI processing logic.

--- src/LogixDb.Data/Transformers/AoiTransformer.cs
+++ src/LogixDb.Data/Transformers/AoiTransformer.cs
@@ -13,7 +13,6 @@ public class AoiTransformer : IDbTransformer
 {
     private readonly AoiMap _aoiMap = new();
     private readonly AoiParameterMap _parameterMap = new();
-    private readonly AoiRungMap _rungMap = new();
 
     /// <inheritdoc />
     public IEnumerable<DataTable> Transform(Target target)
@@ -21,7 +20,6 @@ public class AoiTransformer : IDbTransformer
         var source = target.GetSource();
         var instructions = new List<AddOnInstruction>();
         var parameters = new List<ParameterRecord>();
-        var rungs = new List<AoiRungRecord>();
 
         foreach (var aoi in source.AddOnInstructions)
         {
@@ -29,22 +27,12 @@ public class AoiTransformer : IDbTransformer
             instructions.Add(aoi);
             parameters.AddRange(aoi.Parameters.Select(p => ParameterRecord.FromParameter(p, aoiHash)));
 
-            // Only attempt to process local tags and logic/rungs if the AOI is not encrypted. 
+            // Only attempt to process local tags if the AOI is not encrypted. 
             if (!aoi.IsEncrypted)
-            {
                 parameters.AddRange(aoi.LocalTags.Select(t => ParameterRecord.FromLocalTag(t, aoiHash)));
-
-                rungs.AddRange(
-                    aoi.Routines
-                        .Where(r => r.Type == RoutineType.RLL)
-                        .SelectMany(r => r.Rungs)
-                        .Select(r => AoiRungRecord.FromRung(r, aoiHash))
-                );
-            }
         }
 
         yield return _aoiMap.GenerateTable(instructions);
         yield return _parameterMap.GenerateTable(parameters);
-        yield return _rungMap.GenerateTable(rungs);
     }
 }

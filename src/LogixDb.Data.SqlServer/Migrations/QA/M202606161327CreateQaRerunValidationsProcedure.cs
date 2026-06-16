53c9efcee66f21e3f1d78790c3af37327b7b2123
tnunnink <timothyjnunnink@gmail.com>
2026-06-16 13:47:42 -0500

    Add migration `M202606161327CreateQaRerunValidationsProcedure` for QA rerun validations procedure

---
+++ src/LogixDb.Data.SqlServer/Migrations/QA/M202606161327CreateQaRerunValidationsProcedure.cs
@@ -0,0 +1,20 @@
+using FluentMigrator;
+using JetBrains.Annotations;
+
+namespace LogixDb.Data.SqlServer.Migrations.QA;
+
+[UsedImplicitly]
+[Migration(202606161327, "Create QA rerun validations procedure")]
+[Tags(TagBehavior.RequireAny, MigrationTag.QA)]
+public class M202606161327CreateQaRerunValidationsProcedure : Migration
+{
+    public override void Up()
+    {
+        Execute.EmbeddedScript("QA.016_QaRerunValidationsProcedure.sql");
+    }
+
+    public override void Down()
+    {
+        Execute.Sql("DROP PROCEDURE IF EXISTS [qa].[rerun_validations]");
+    }
+}

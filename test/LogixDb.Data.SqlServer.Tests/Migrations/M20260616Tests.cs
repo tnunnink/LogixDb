5782be784372f3744f10153f78388d2c38e6e874
tnunnink <timothyjnunnink@gmail.com>
2026-06-17 21:00:16 -0500

    Add unit tests for `M202606162100CreateTraceabilityTables` migration in SQL Server and SQLite contexts

--- test/LogixDb.Data.SqlServer.Tests/Migrations/M20260616Tests.cs
+++ test/LogixDb.Data.SqlServer.Tests/Migrations/M20260616Tests.cs
@@ -18,4 +18,35 @@ public class M20260616Tests : SqlServerTestFixture
 
         await AssertFunctionExists("get_latest_version_id");
     }
+
+    [Test]
+    public async Task MigrateUp_ToM202606162100_CreatesTraceabilityTablesWithExpectedColumns()
+    {
+        await Database.Migrate(202606162100, ComponentOptions.All);
+
+        using (Assert.EnterMultipleScope())
+        {
+            // Assert import table
+            await AssertTableExists("import");
+            await AssertColumnDefinition("import", "import_id", "uniqueidentifier");
+            await AssertColumnDefinition("import", "import_status", "nvarchar");
+            await AssertColumnDefinition("import", "source_type", "nvarchar");
+            await AssertColumnDefinition("import", "file_type", "nvarchar");
+            await AssertColumnDefinition("import", "file_name", "nvarchar");
+            await AssertColumnDefinition("import", "posted_on", "datetime");
+            await AssertPrimaryKey("import", "import_id");
+
+            // Assert import_log table
+            await AssertTableExists("import_log");
+            await AssertColumnDefinition("import_log", "log_id", "bigint");
+            await AssertColumnDefinition("import_log", "import_id", "uniqueidentifier");
+            await AssertColumnDefinition("import_log", "timestamp", "datetime");
+            await AssertColumnDefinition("import_log", "log_severity", "nvarchar");
+            await AssertColumnDefinition("import_log", "log_message", "nvarchar");
+            await AssertColumnDefinition("import_log", "log_exception", "nvarchar");
+            await AssertPrimaryKey("import_log", "log_id");
+            await AssertForeignKey("import_log", "import_id", "import", "import_id");
+            await AssertIndex("import_log", "import_id");
+        }
+    }
 }
---
+++ test/LogixDb.Data.Sqlite.Tests/Migrations/M20260616Tests.cs
@@ -0,0 +1,36 @@
+namespace LogixDb.Data.Sqlite.Tests.Migrations;
+
+[TestFixture]
+public class M20260616Tests : SqliteTestFixture
+{
+    [Test]
+    public async Task MigrateUp_ToM202606162100_CreatesTraceabilityTablesWithExpectedColumns()
+    {
+        await Database.Migrate(202606162100, ComponentOptions.All);
+
+        using (Assert.EnterMultipleScope())
+        {
+            // Assert import table
+            await AssertTableExists("import");
+            await AssertColumnDefinition("import", "import_id", "UNIQUEIDENTIFIER");
+            await AssertColumnDefinition("import", "import_status", "text");
+            await AssertColumnDefinition("import", "source_type", "text");
+            await AssertColumnDefinition("import", "file_type", "text");
+            await AssertColumnDefinition("import", "file_name", "text");
+            await AssertColumnDefinition("import", "posted_on", "datetime");
+            await AssertPrimaryKey("import", "import_id");
+
+            // Assert import_log table
+            await AssertTableExists("import_log");
+            await AssertColumnDefinition("import_log", "log_id", "integer");
+            await AssertColumnDefinition("import_log", "import_id", "UNIQUEIDENTIFIER");
+            await AssertColumnDefinition("import_log", "timestamp", "datetime");
+            await AssertColumnDefinition("import_log", "log_severity", "text");
+            await AssertColumnDefinition("import_log", "log_message", "text");
+            await AssertColumnDefinition("import_log", "log_exception", "text");
+            await AssertPrimaryKey("import_log", "log_id");
+            await AssertForeignKey("import_log", "import_id", "import", "import_id");
+            await AssertIndex("import_log", "import_id");
+        }
+    }
+}

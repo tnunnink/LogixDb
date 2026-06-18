764e29c8f6d10b7b11919a9539e56e93a2904dc4
tnunnink <timothyjnunnink@gmail.com>
2026-06-17 21:31:08 -0500

    Update `.idea` workspace configuration to reflect recent changes in scripts, logging, and metadata settings.

--- src/LogixDb.Data.SqlServer/Scripts/DBO/PutImport.sql
+++ src/LogixDb.Data.SqlServer/Scripts/DBO/PutImport.sql
@@ -6,4 +6,4 @@ WHEN MATCHED THEN
         import_status = @ImportStatus
 WHEN NOT MATCHED THEN
     INSERT (import_id, import_status, source_type, file_type, file_name)
-    VALUES (@ImportId, @Status, @SourceType, @FileType, @FileName);
+    VALUES (@ImportId, @ImportStatus, @SourceType, @FileType, @FileName);

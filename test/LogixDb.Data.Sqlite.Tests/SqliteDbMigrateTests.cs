d63f2a1aaf56bed1fdc1eff6de10a8fee95effc0
tnunnink <timothyjnunnink@gmail.com>
2026-06-19 15:47:44 -0500

    Remove redundant SQLite migration tests and apply minor formatting improvements to `MergeTagValue.sql`.

--- test/LogixDb.Data.Sqlite.Tests/SqliteDbMigrateTests.cs
+++ test/LogixDb.Data.Sqlite.Tests/SqliteDbMigrateTests.cs
@@ -15,118 +15,12 @@ public class SqliteDbMigrateTests : SqliteTestFixture
     {
         var connection = new DbConnectionInfo(DbProvider.Sqlite, "../../../logix.db");
 
-        if (File.Exists(connection.Source)) 
+        if (File.Exists(connection.Source))
             File.Delete(connection.Source);
-        
+
         var result = await Migrator.Migrate(connection);
-        
+
         Assert.That(result.Success, Is.True);
         FileAssert.Exists("../../../logix.db");
-    }
-
-    [Test]
-    public async Task Migrate_OnlyControllerAndTagBasedTables_ShouldOnlyHaveExpectedTables()
-    {
-        await Migrator.Migrate(Connection);
-
-        // Required Tables
-        await AssertTableExists("target");
-        await AssertTableExists("target_version");
-        await AssertTableExists("target_version_map");
-        await AssertTableExists("target_info");
-
-        // Included Tables
-        await AssertTableExists("controller");
-        await AssertTableExists("task");
-        await AssertTableExists("program");
-        await AssertTableExists("tag");
-        await AssertTableExists("tag_member");
-        await AssertTableExists("tag_member_comment");
-        await AssertTableExists("tag_value");
-        await AssertTableExists("tag_producer");
-        await AssertTableExists("tag_consumer");
-
-        // Excluded Tables
-        await AssertTableDoesNotExists("data_type");
-        await AssertTableDoesNotExists("data_type_member");
-        await AssertTableDoesNotExists("module");
-        await AssertTableDoesNotExists("routine");
-        await AssertTableDoesNotExists("rung");
-        await AssertTableDoesNotExists("rung_instruction");
-        await AssertTableDoesNotExists("rung_argument");
-        await AssertTableDoesNotExists("rung_reference");
-        await AssertTableDoesNotExists("aoi");
-        await AssertTableDoesNotExists("aoi_parameter");
-        await AssertTableDoesNotExists("operand");
-    }
-
-    [Test]
-    public async Task Migrate_OnlyLogicBasedTables_ShouldOnlyHaveLogicTables()
-    {
-        await Migrator.Migrate(Connection);
-
-        // Required Tables
-        await AssertTableExists("target");
-        await AssertTableExists("target_version");
-        await AssertTableExists("target_version_map");
-        await AssertTableExists("target_info");
-
-        // Included Tables
-        await AssertTableExists("task");
-        await AssertTableExists("program");
-        await AssertTableExists("routine");
-        await AssertTableExists("rung");
-        await AssertTableExists("rung_instruction");
-        await AssertTableExists("rung_argument");
-        await AssertTableExists("rung_reference");
-        await AssertTableExists("operand");
-
-        // Excluded Tables
-        await AssertTableDoesNotExists("controller");
-        await AssertTableDoesNotExists("data_type");
-        await AssertTableDoesNotExists("data_type_member");
-        await AssertTableDoesNotExists("module");
-        await AssertTableDoesNotExists("aoi");
-        await AssertTableDoesNotExists("aoi_parameter");
-        await AssertTableDoesNotExists("tag");
-        await AssertTableDoesNotExists("tag_member");
-        await AssertTableDoesNotExists("tag_member_comment");
-        await AssertTableDoesNotExists("tag_producer");
-        await AssertTableDoesNotExists("tag_consumer");
-        await AssertTableDoesNotExists("tag_value");
-    }
-
-    [Test]
-    public async Task Migrate_NoComponentTables_ShouldHaveNoComponentTables()
-    {
-        await Migrator.Migrate(Connection);
-
-        // Required Tables
-        await AssertTableExists("target");
-        await AssertTableExists("target_version");
-        await AssertTableExists("target_version_map");
-        await AssertTableExists("target_info");
-
-        // Excluded Tables
-        await AssertTableDoesNotExists("task");
-        await AssertTableDoesNotExists("program");
-        await AssertTableDoesNotExists("routine");
-        await AssertTableDoesNotExists("rung");
-        await AssertTableDoesNotExists("rung_instruction");
-        await AssertTableDoesNotExists("rung_argument");
-        await AssertTableDoesNotExists("rung_reference");
-        await AssertTableDoesNotExists("operand");
-        await AssertTableDoesNotExists("controller");
-        await AssertTableDoesNotExists("data_type");
-        await AssertTableDoesNotExists("data_type_member");
-        await AssertTableDoesNotExists("module");
-        await AssertTableDoesNotExists("aoi");
-        await AssertTableDoesNotExists("aoi_parameter");
-        await AssertTableDoesNotExists("tag");
-        await AssertTableDoesNotExists("tag_member");
-        await AssertTableDoesNotExists("tag_member_comment");
-        await AssertTableDoesNotExists("tag_producer");
-        await AssertTableDoesNotExists("tag_consumer");
-        await AssertTableDoesNotExists("tag_value");
     }
 }
